3391280d4acf13a65de515c617390f4c9275b688
srdtrk <srdtrk@hotmail.com>
2023-03-10 13:36:41 +0300

    added method to check nft permissions

--- src/permit.rs
+++ src/permit.rs
@@ -1,20 +1,21 @@
 use schemars::JsonSchema;
+use secret_toolkit::permit::Permit;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum NftPermissions {
-    /// Allowance for SNIP-20 - Permission to query allowance of the owner & spender
-    Allowance,
     /// Balance for SNIP-721 - Permission to query all nft balance of the permit creator
     Balance,
-    /// Balances of listed token_ids for SNIP-721 - Permission to query the nft balances of the given token_ids
+    /// Balances of listed token_ids for SNIP-721
+    /// Permission to query the nft balances of the given token_ids as the permit creator
     BalanceOf(Vec<String>),
     /// History for SNIP-721 - Permission to query transfer_history & transaction_history
     History,
     /// Metadata for SNIP-721 - Permission to query private metadata of all NFTs owned by the permit creator
     Metadata,
-    /// Metadata of listed token_ids for SNIP-721 - Permission to query private metadata of the given token_ids
+    /// Metadata of listed token_ids for SNIP-721
+    /// Permission to query private metadata of the given token_ids as the permit creator
     MetadataOf(Vec<String>),
     /// Owner permission indicates that the bearer of this permit should be granted all
     /// the access of the creator/signer of the permit.  SNIP-721 uses this to grant
@@ -24,4 +25,30 @@ pub enum NftPermissions {
     /// the address they want to share with via a SetWhitelistedApproval tx, and that
     /// address will view the data by creating their own permit with Owner permission
     Owner,
+}
+
+pub fn check_nft_permission(permit: &Permit<NftPermissions>, permission: &NftPermissions) -> bool {
+    let permit_permissions: &Vec<NftPermissions> = &permit.params.permissions;
+
+    match permission {
+        NftPermissions::BalanceOf(token_ids) => {
+            return permit_permissions.iter().any(|permit_permission| {
+                if let NftPermissions::BalanceOf(permitted_ids) = permit_permission {
+                    token_ids.iter().all(|id| permitted_ids.contains(id))
+                } else {
+                    false
+                }
+            });
+        }
+        NftPermissions::MetadataOf(token_ids) => {
+            return permit_permissions.iter().any(|permit_permission| {
+                if let NftPermissions::MetadataOf(permitted_ids) = permit_permission {
+                    token_ids.iter().all(|id| permitted_ids.contains(id))
+                } else {
+                    false
+                }
+            });
+        }
+        _ => permit.params.permissions.contains(permission),
+    }
 }
